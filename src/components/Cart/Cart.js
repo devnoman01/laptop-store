3b220d27dddad6cc39df697b25e27de0eea42799
devnoman01 <devnoman01@gmail.com>
2022-03-26 21:23:42 +0600

    Cart component complete

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRemove } from "@fortawesome/free-solid-svg-icons";
 import "./Cart.css";
 
-const Cart = () => {
-  return <div></div>;
+const Cart = (props) => {
+  const { name, picture } = props.cart;
+
+  return (
+    <div className="row p-3">
+      <div className="list-parent border">
+        <img src={picture} alt="" />
+        <p>{name}</p>
+        <FontAwesomeIcon
+          className="me-4 removeBtn"
+          icon={faRemove}
+        ></FontAwesomeIcon>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
