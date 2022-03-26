47e1a7e396eaa9bc8adced54d46f092ebcb1c2e2
devnoman01 <devnoman01@gmail.com>
2022-03-26 21:20:44 +0600

    Products component complete

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,8 +1,30 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
 import "./Products.css";
 
-const Products = () => {
-  return <div></div>;
+const Products = (props) => {
+  const { product, handleAddToCart } = props;
+  const { name, price, picture } = product;
+  return (
+    <div className="col">
+      <div className="card h-100 py-3">
+        <img src={picture} className="card-img-top p-3" alt="..." />
+        <div className="card-body">
+          <h4 className="card-title">{name}</h4>
+          <p className="card-text fs-5">${price}</p>
+        </div>
+        <div className="card-footer">
+          <button
+            onClick={() => handleAddToCart(product)}
+            className="btn addToCartBtn"
+          >
+            Add to Cart <FontAwesomeIcon icon={faCartPlus}></FontAwesomeIcon>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Products;
